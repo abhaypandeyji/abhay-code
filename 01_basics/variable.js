dd554f29fb0e83e5e26773945f18884aba42cf56
Abhay Kumar Pandey <137700321+abhaypandeyji@users.noreply.github.com>
2023-08-13 16:12:21 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,4 @@
+let a = 12;
+let b = 13;
+console.log(a+b);
+console.table([a,b]);
--- abhay.java
+++
@@ -1,8 +0,0 @@
-class Abhay{
-  public static void main(String[] args){
-    int a = 12;
-    int b = 13;
-    System.out.println(a+b);
-  }
-}  
-  
