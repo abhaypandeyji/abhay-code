5dba144a4aec466d429bdd0713e3ed6e9bcf0447
Abhay Kumar Pandey <137700321+abhaypandeyji@users.noreply.github.com>
2023-08-14 17:43:51 +0530

    data type done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+let name = "krishna";
+let isLoggedIn = false;
+let state = null;  //null data types contains only one value that is null
+console.log(typeof name);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+
+
