683c397c2cbfb4af34e65d00daf83ade7078d9aa
Abhay Kumar Pandey <137700321+abhaypandeyji@users.noreply.github.com>
2023-08-20 17:46:56 +0000

    array done

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -7,5 +7,5 @@ marvel_heros.push(dc_heros);
 // console.log(marvel_heros[3][1]);
 // console.log(marvel_heros.length);  //length = 4;
 
-marvel_heros.concat(dc_heros);
-console.log(marvel_heros);
+const allHeros =  marvel_heros.concat(dc_heros);
+console.log(allHeros);
