1171384bacbf3b7f1fe1945061609c2dd410d795
Abhay Kumar Pandey <137700321+abhaypandeyji@users.noreply.github.com>
2023-09-16 09:21:24 +0000

    done

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -72,10 +72,31 @@ async function consumePromiseFive(){
     try{
         const response = await promiseFive
         console.log(response);
-    }catch(error) {
+    }
+    catch(error) {
         console.log(error)
     }
 }
-
 consumePromiseFive()
 
+// async function getAllUsers(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         //console.log(response);
+        
+//         const data = await response.json();
+//         console.log(data);
+//     }
+//     catch(error){
+//         console.log("E: ",error);
+//     }
+// }
+
+// getAllUsers();
+
+fetch('https://api.github.com/users/abhaypandeyji')
+.then((response) => {
+    return response.json()
+}).then( (data) => {
+    console.log(data);
+});
